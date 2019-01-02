7356d0880ace4a2b598f73a8066bffa63f6aa93e
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-02 15:42:37 -0800

    "Edited object to fix trailing commas, functions, and undefined values"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -13,7 +13,7 @@ var stringifyJSON = function(obj) {
     for (let i = 0; i < obj.length; i++) {
       jsonString += stringifyJSON(obj[i]);
       if (i !== obj.length - 1) {
-        jsonString += ', ';
+        jsonString += ',';
       }
     }
     jsonString += ']';
@@ -21,17 +21,25 @@ var stringifyJSON = function(obj) {
 
   } else if (obj && typeof obj === 'object') { //testing if not null 
     jsonString += '{';
+    
     for (let key in obj) {
-      jsonString += '"' + key + '": ' + stringifyJSON(obj[key]);
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        //do nothing - ignore value
+      } else {
+        jsonString += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+
+    if (jsonString.length > 1 ) {
+      jsonString = jsonString.slice(0, jsonString.length - 1);
     }
     jsonString += '}';
     return jsonString;
   } else {
     
-    let stringObj = obj.toString();
     //add number
-    if (stringObj[0] === '-' || stringObj[0] >= 0 || stringObj[0] >= 9) {
-      return stringObj;
+    if (typeof obj === 'number') {
+      return jsonString.concat(obj);
     }
     
     //add string
@@ -46,15 +54,21 @@ var stringifyJSON = function(obj) {
     if (obj === null) {
       return 'null';      
     }
-    if (obj === undefined || typeof obj === "function") {
+    if (obj === undefined || typeof obj === 'function') {
       return '';
     }
-    console.log("not found (err)");
-    return "error";
+    console.log('not found (err)');
+    return 'error';
   }
   /*
 function: 
 undefined
 */
 };
+
+
+
+
+
+
 
