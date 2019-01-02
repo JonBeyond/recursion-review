99fc47846091c01690a03e7e7dbfdd848ff1ec4c
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-02 14:55:36 -0800

    "Added in object stringify"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,12 +7,34 @@ var stringifyJSON = function(obj) {
   // your code goes here
   let jsonString = '';
   //if obj is an array 
-  if(Array.isArray(obj)){
-    for(let i = 0; i < obj.length; i++){
-    
+  if (Array.isArray(obj)) {
+    jsonString += '[';
+    for (let i = 0; i < obj.length; i++) {
+      jsonString += stringifyJSON(obj[i]);
+      if (i !== obj.length - 1) {
+        jsonString += ', ';
+      }
     }
+    jsonString += ']';
+    return jsonString;
+
+  } else if (typeof obj === 'object') {
+    jsonString += '{';
+    for (let key in obj) {
+      jsonString += '"' + key + '": ' + stringifyJSON(obj[key]);
+    }
+    jsonString += '}';
+    return jsonString;
+  
   }
-  //case if obj is an object
-  //case if obj is a primitive 
+
+
+
+
+  //case if obj is an objec
+
+
+
+  //case if obj is a primitive : null, num, string 
 };
 
