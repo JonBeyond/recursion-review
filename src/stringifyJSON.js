4dcc7d1b569087da1dacf4375901ed26bb1d7b92
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-02 15:11:33 -0800

    "Added number base case"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,6 +7,7 @@ var stringifyJSON = function(obj) {
   // your code goes here
   let jsonString = '';
   //if obj is an array 
+  
   if (Array.isArray(obj)) {
     jsonString += '[';
     for (let i = 0; i < obj.length; i++) {
@@ -18,16 +19,34 @@ var stringifyJSON = function(obj) {
     jsonString += ']';
     return jsonString;
 
-  } else if (typeof obj === 'object') {
+  } else if (obj && typeof obj === 'object') { //testing if not null 
     jsonString += '{';
     for (let key in obj) {
       jsonString += '"' + key + '": ' + stringifyJSON(obj[key]);
     }
     jsonString += '}';
     return jsonString;
-  
+  } else {
+    let stringObj = obj.toString();
+    
+    //add number
+    if (stringObj[0] === '-' || stringObj[0] >= 0 || stringObj[0] >= 9) {
+      return stringObj;
+    }
+    
+    
+    
   }
+  /*
+number: can start with 0 to 9 or a - (negative)
+string: will start with " and end with a 
+null: 
+boolean: true / false 
+function: 
+undefined
 
+
+*/
 
 
 
