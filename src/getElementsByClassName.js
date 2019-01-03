e33268da595e9c3bbd110789af971dc3c129d98e
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-02 18:17:28 -0800

    "Created basic structure for base case and recursive case"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, prenode) {
+  let node = prenode || body.document;
+  let elementCollection = [];
+
+  if (node.classList.contains(className)) {
+    //do a thing. add to master object
+    elementCollection.push(node);
+  }
+  if (node.childNodes) {
+    //dive deeper
+//look more into child nodes
+// then figure out how to determine if it exists
+    getElementsByClassName(className, node.childNodes() );
+  }
+  // check node contains className
+  // -> check if node contains child getElements
+  // --> recall recursively node.childNodes()
+  
+  
+  
 };
+
+/*
+Useful methods:
+
+node.classList.contains()
+
+
+
+
+*/
